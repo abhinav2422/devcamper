d9e7ba038de3ac4789ac562aac3656293e92a3d1
Abhinav Jain <jabhi678@gmail.com>
2020-01-23 18:33:05 +0530

    db connected

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Route Files
 const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env' });
+
+//Connect DB
+connectDB();
 
 const app = express();
 
@@ -18,7 +22,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
 );
+
+//Handle Rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Exit app
+  server.close(() => process.exit(1));
+});
